ec476e1da47d1c8a0c5dbd57f0d9bad9a6502b86
Denley <936489270@qq.com>
2019-05-03 12:39:48 +0800

    Initial commit

---
+++ src/main/java/org/sonatype/mavenbook/ch03/App.java
@@ -0,0 +1,39 @@
+package org.sonatype.mavenbook.ch03;
+
+/**
+ * @description : 
+ * @author : Denley
+ * @date : 2019年5月2日 12:29
+ **/
+public class App {
+
+    private int a = 0;
+    private Object o = new Object();
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String bbb = "aaa";
+        System.out.println(bbb);
+        int b = 1;
+        int c = test(b);
+        System.out.println(c);
+        if(c > 0){
+            System.out.println(111);
+        }
+    }
+
+
+    // 爱你
+    /**
+     * @description : 
+     * @author : Denley
+     * @date : 2019年5月2日 12:29
+     * @Param : [b]
+     * @return : int
+     **/
+    private static int test(int b)  {
+        int a = 111;
+        return b + a;
+    }
+
+}
---
+++ src/main/java/org/sonatype/mavenbook/ch03/HelloWorld.java
@@ -0,0 +1,21 @@
+package org.sonatype.mavenbook.ch03;
+
+/**
+ * @author : Denley
+ * @version : 1.0
+ * @className : HelloWorld
+ * @description : TODO
+ * @date : 2019-05-02 12:19:25
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
+
+
+
+
+
+
+
+}
