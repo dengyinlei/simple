9549900581de36fd5d3b66af7716da810dc0acc8
Denley <936489270@qq.com>
2019-05-03 21:47:21 +0800

    second commit

---
+++ src/main/java/org/sonatype/mavenbook/ch03/HelloDenley.java
@@ -0,0 +1,16 @@
+package org.sonatype.mavenbook.ch03;
+
+/**
+ * @author : Denley
+ * @version : 1.0
+ * @className : HelloDenley
+ * @description : TODO
+ * @date : 2019-05-03 21:38:12
+ **/
+public class HelloDenley {
+    public static void main(String[] args) {
+        System.out.println(111);
+        System.out.println(222);
+        System.out.println(222222);
+    }
+}
--- src/main/java/org/sonatype/mavenbook/ch03/HelloTom.java
+++ src/main/java/org/sonatype/mavenbook/ch03/HelloTom.java
@@ -9,5 +9,7 @@ package org.sonatype.mavenbook.ch03;
  **/
 public class HelloTom {
 
-
+    public static void main(String[] args) {
+        System.out.println(1111);
+    }
 }
